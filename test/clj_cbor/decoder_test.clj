2fff3151938d87ac35f4ad41242ac450b25c4ef3
Greg Look <greg@greg-look.net>
2016-10-12 01:20:59 -0700

    Upcase hex constants in decoder tests.

--- test/clj_cbor/decoder_test.clj
+++ test/clj_cbor/decoder_test.clj
@@ -23,19 +23,19 @@
   (testing "direct values"
     (is (= 0 (decode-hex "00")))
     (is (= 1 (decode-hex "01")))
-    (is (= 10 (decode-hex "0a")))
+    (is (= 10 (decode-hex "0A")))
     (is (= 23 (decode-hex "17"))))
   (testing "uint8"
     (is (= 24 (decode-hex "1818")))
     (is (= 25 (decode-hex "1819")))
     (is (= 100 (decode-hex "1864"))))
   (testing "uint16"
-    (is (= 1000 (decode-hex "1903e8"))))
+    (is (= 1000 (decode-hex "1903E8"))))
   (testing "uint32"
-    (is (= 1000000 (decode-hex "1a000f4240"))))
+    (is (= 1000000 (decode-hex "1A000F4240"))))
   (testing "uint64"
-    (is (= 1000000000000 (decode-hex "1b000000e8d4a51000")))
-    (is (= 18446744073709551615 (decode-hex "1bffffffffffffffff")))))
+    (is (= 1000000000000 (decode-hex "1B000000E8D4A51000")))
+    (is (= 18446744073709551615 (decode-hex "1BFFFFFFFFFFFFFFFF")))))
 
 
 (deftest negative-integers
@@ -44,57 +44,57 @@
     (is (= -10 (decode-hex "29"))))
   (testing "int8"
     (is (= -100 (decode-hex "3863")))
-    (is (= -1000 (decode-hex "3903e7"))))
+    (is (= -1000 (decode-hex "3903E7"))))
   (testing "int64"
-    (is (= -18446744073709551616 (decode-hex "3bffffffffffffffff")))))
+    (is (= -18446744073709551616 (decode-hex "3BFFFFFFFFFFFFFFFF")))))
 
 
 (deftest floating-point-numbers
   (testing "half-precision"
-    (is (instance? Float (decode-hex "f90000")))
-    (is (= 0.0 (decode-hex "f90000")))
-    (is (= -0.0 (decode-hex "f98000")))
-    (is (= 1.0 (decode-hex "f93c00")))
-    (is (= 1.5 (decode-hex "f93e00")))
-    (is (= 65504.0 (decode-hex "f97bff")))
-    (is (= 5.960464477539063e-8 (decode-hex "f90001")))
-    (is (= 0.00006103515625 (decode-hex "f90400")))
-    (is (= -4.0 (decode-hex "f9c400")))
-    (is (Float/isNaN (decode-hex "f97e00")))
-    (is (= Float/POSITIVE_INFINITY (decode-hex "f97c00")))
-    (is (= Float/NEGATIVE_INFINITY (decode-hex "f9fc00"))))
+    (is (instance? Float (decode-hex "F90000")))
+    (is (= 0.0 (decode-hex "F90000")))
+    (is (= -0.0 (decode-hex "F98000")))
+    (is (= 1.0 (decode-hex "F93C00")))
+    (is (= 1.5 (decode-hex "F93E00")))
+    (is (= 65504.0 (decode-hex "F97BFF")))
+    (is (= 5.960464477539063e-8 (decode-hex "F90001")))
+    (is (= 0.00006103515625 (decode-hex "F90400")))
+    (is (= -4.0 (decode-hex "F9C400")))
+    (is (Float/isNaN (decode-hex "F97E00")))
+    (is (= Float/POSITIVE_INFINITY (decode-hex "F97C00")))
+    (is (= Float/NEGATIVE_INFINITY (decode-hex "F9FC00"))))
   (testing "single-precision"
-    (is (instance? Float (decode-hex "fa47c35000")))
-    (is (= 100000.0 (decode-hex "fa47c35000")))
-    (is (= 3.4028234663852886e+38 (decode-hex "fa7f7fffff")))
-    (is (Float/isNaN (decode-hex "fa7fc00000")))
-    (is (= Float/POSITIVE_INFINITY (decode-hex "fa7f800000")))
-    (is (= Float/NEGATIVE_INFINITY (decode-hex "faff800000"))))
+    (is (instance? Float (decode-hex "FA47C35000")))
+    (is (= 100000.0 (decode-hex "FA47C35000")))
+    (is (= 3.4028234663852886e+38 (decode-hex "FA7F7FFFFF")))
+    (is (Float/isNaN (decode-hex "FA7FC00000")))
+    (is (= Float/POSITIVE_INFINITY (decode-hex "FA7F800000")))
+    (is (= Float/NEGATIVE_INFINITY (decode-hex "FAFF800000"))))
   (testing "double-precision"
-    (is (instance? Double (decode-hex "fb7ff8000000000000")))
-    (is (= 1.1 (decode-hex "fb3ff199999999999a")))
-    (is (= 1.0e+300 (decode-hex "fb7e37e43c8800759c")))
-    (is (= -4.1 (decode-hex "fbc010666666666666")))
-    (is (Double/isNaN (decode-hex "fb7ff8000000000000")))
-    (is (= Double/POSITIVE_INFINITY (decode-hex "fb7ff0000000000000")))
-    (is (= Double/NEGATIVE_INFINITY (decode-hex "fbfff0000000000000")))))
+    (is (instance? Double (decode-hex "FB7FF8000000000000")))
+    (is (= 1.1 (decode-hex "FB3FF199999999999A")))
+    (is (= 1.0e+300 (decode-hex "FB7E37E43C8800759C")))
+    (is (= -4.1 (decode-hex "FBC010666666666666")))
+    (is (Double/isNaN (decode-hex "FB7FF8000000000000")))
+    (is (= Double/POSITIVE_INFINITY (decode-hex "FB7FF0000000000000")))
+    (is (= Double/NEGATIVE_INFINITY (decode-hex "FBFFF0000000000000")))))
 
 
 (deftest simple-values
-  (is (false? (decode-hex "f4")))
-  (is (true? (decode-hex "f5")))
-  (is (nil? (decode-hex "f6")))
-  (is (= data/undefined (decode-hex "f7")))
-  (is (= (data/simple-value 16) (decode-hex "f0")))
-  (is (= (data/simple-value 24) (decode-hex "f818")))
-  (is (= (data/simple-value 255) (decode-hex "f8ff"))))
+  (is (false? (decode-hex "F4")))
+  (is (true? (decode-hex "F5")))
+  (is (nil? (decode-hex "F6")))
+  (is (= data/undefined (decode-hex "F7")))
+  (is (= (data/simple-value 16) (decode-hex "F0")))
+  (is (= (data/simple-value 24) (decode-hex "F818")))
+  (is (= (data/simple-value 255) (decode-hex "F8FF"))))
 
 
 #_
 (deftest tagged-values
   (testing "bignum"
-    (is (=  18446744073709551616N (decode-hex "c249010000000000000000")))
-    (is (= -18446744073709551617N (decode-hex "c349010000000000000000"))))
+    (is (=  18446744073709551616N (decode-hex "C249010000000000000000")))
+    (is (= -18446744073709551617N (decode-hex "C349010000000000000000"))))
 
 ;  | 0("2013-03-21T20:04:00Z")    | 0xc074323031332d30332d32315432303a |
 ;  |                              | 30343a30305a                       |
@@ -115,18 +115,18 @@
 (deftest byte-strings
   (is (bytes= [] (decode-hex "40")))
   (is (bytes= [1 2 3 4] (decode-hex "4401020304")))
-  (is (bytes= [1 2 3 4 5] (decode-hex "5f42010243030405ff"))))
+  (is (bytes= [1 2 3 4 5] (decode-hex "5F42010243030405FF"))))
 
 
 (deftest text-strings
   (is (= "" (decode-hex "60")))
   (is (= "a" (decode-hex "6161")))
   (is (= "IETF" (decode-hex "6449455446")))
-  (is (= "\"\\" (decode-hex "62225c")))
-  (is (= "\u00fc" (decode-hex "62c3bc")))
-  (is (= "\u6c34" (decode-hex "63e6b0b4")))
-  (is (= "\ud800\udd51" (decode-hex "64f0908591")))
-  (is (= "streaming" (decode-hex "7f657374726561646d696e67ff"))))
+  (is (= "\"\\" (decode-hex "62225C")))
+  (is (= "\u00fc" (decode-hex "62C3BC")))
+  (is (= "\u6c34" (decode-hex "63E6B0B4")))
+  (is (= "\ud800\udd51" (decode-hex "64F0908591")))
+  (is (= "streaming" (decode-hex "7F657374726561646D696E67FF"))))
 
 
 (deftest data-arrays
@@ -134,24 +134,24 @@
   (is (= [1 2 3] (decode-hex "83010203")))
   (is (= [1 [2 3] [4 5]] (decode-hex "8301820203820405")))
   (is (= [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25]
-         (decode-hex "98190102030405060708090a0b0c0d0e0f101112131415161718181819")))
+         (decode-hex "98190102030405060708090A0B0C0D0E0F101112131415161718181819")))
   (testing "streaming"
-    (is (= [] (decode-hex "9fff")))
-    (is (= [1 [2 3] [4 5]] (decode-hex "9f018202039f0405ffff")))
-    (is (= [1 [2 3] [4 5]] (decode-hex "9f01820203820405ff")))
-    (is (= [1 [2 3] [4 5]] (decode-hex "83018202039f0405ff")))
-    (is (= [1 [2 3] [4 5]] (decode-hex "83019f0203ff820405")))
-    (is (= (range 1 26) (decode-hex "9f0102030405060708090a0b0c0d0e0f101112131415161718181819ff")))))
+    (is (= [] (decode-hex "9FFF")))
+    (is (= [1 [2 3] [4 5]] (decode-hex "9F018202039F0405FFFF")))
+    (is (= [1 [2 3] [4 5]] (decode-hex "9F01820203820405FF")))
+    (is (= [1 [2 3] [4 5]] (decode-hex "83018202039F0405FF")))
+    (is (= [1 [2 3] [4 5]] (decode-hex "83019F0203FF820405")))
+    (is (= (range 1 26) (decode-hex "9F0102030405060708090A0B0C0D0E0F101112131415161718181819FF")))))
 
 
 (deftest data-maps
-  (is (= {} (decode-hex "a0")))
-  (is (= {1 2, 3 4} (decode-hex "a201020304")))
-  (is (= {"a" 1, "b" [2 3]} (decode-hex "a26161016162820203")))
-  (is (= ["a" {"b" "c"}] (decode-hex "826161a161626163")))
+  (is (= {} (decode-hex "A0")))
+  (is (= {1 2, 3 4} (decode-hex "A201020304")))
+  (is (= {"a" 1, "b" [2 3]} (decode-hex "A26161016162820203")))
+  (is (= ["a" {"b" "c"}] (decode-hex "826161A161626163")))
   (is (= {"a" "A", "b" "B", "c" "C", "d" "D", "e" "E"}
-         (decode-hex "a56161614161626142616361436164614461656145")))
+         (decode-hex "A56161614161626142616361436164614461656145")))
   (testing "streaming"
-    (is (= {"a" 1, "b" [2 3]} (decode-hex "bf61610161629f0203ffff")))
-    (is (= ["a" {"b" "c"}] (decode-hex "826161bf61626163ff")))
-    (is (= {"Fun" true, "Amt" -2} (decode-hex "bf6346756ef563416d7421ff")))))
+    (is (= {"a" 1, "b" [2 3]} (decode-hex "BF61610161629F0203FFFF")))
+    (is (= ["a" {"b" "c"}] (decode-hex "826161BF61626163FF")))
+    (is (= {"Fun" true, "Amt" -2} (decode-hex "BF6346756EF563416D7421FF")))))
