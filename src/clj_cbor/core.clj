158454a621004945dd92bb675aec09d5f87fb734
Greg Look <greg@greg-look.net>
2016-12-17 13:49:14 -0800

    Document main API functions.

--- src/clj_cbor/core.clj
+++ src/clj_cbor/core.clj
@@ -18,21 +18,40 @@
 
 
 (defn cbor-codec
+  "Constructs a new CBOR codec.
+
+  Valid options are:
+
+  - `:formatter-dispatch` function which is called to provide a dispatch value
+    based on the data to be rendered. (default: `class`)
+  - `:formatters` map of dispatch values to _formatting functions_ which take
+    some data to be encoded and return a transformed version of it (typically
+    tagged values).
+  - `:tag-handlers` map of integer tags to _handler functions_ which take the
+    embedded item and return the transformed data value."
   [& {:as opts}]
   (codec/map->CBORCodec
     (merge
       {:formatter-dispatch class
        :formatters (merge numbers/bignum-formatters
-                          time/date-time-string-formatters)
+                          time/time-epoch-formatters)
        :tag-handlers (merge numbers/bignum-handlers
                             time/instant-handlers)}
       opts)))
 
 
+(def default-codec
+  "Default CBOR codec to use when none is specified."
+  (cbor-codec))
+
+
 (defn encode
-  "..."
+  "Encodes a value as CBOR data. In the general form, this writes a value to an
+  output stream and returns the number of bytes written. If omitted, the
+  function returns a byte array instead. Uses the `default-codec` if none is
+  provided."
   ([value]
-   (encode (codec/map->CBORCodec nil) value))
+   (encode default-codec value))
   ([encoder value]
    (let [buffer (ByteArrayOutputStream.)]
      (encode encoder buffer value)
@@ -43,10 +62,14 @@
 
 
 (defn decode
-  "Returns a lazy sequence of values decoded from the CBOR input stream. The
-  sequence will end with the end of the data."
+  "Decodes a sequence of CBOR values from the given input data, which may be a
+  byte array or coercible to `input-stream`. Uses the `default-codec` if none
+  is provided.
+
+  This returns a lazy sequence, so take care that the input stream is not
+  closed before the entries are realized."
   ([input]
-   (decode (codec/map->CBORCodec nil) input))
+   (decode default-codec input))
   ([decoder input]
    (let [eof-guard (Object.)
          data-input (DataInputStream.
