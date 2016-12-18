a536f98373946393cfc78b44b1d37a4dde6d28db
Greg Look <greg@greg-look.net>
2016-12-18 14:42:10 -0800

    Implement tagged decimal fractions.

--- src/clj_cbor/tags/numbers.clj
+++ src/clj_cbor/tags/numbers.clj
@@ -25,7 +25,10 @@
 
 (defn parse-big-int
   [^long tag ^bytes value]
-  ; TODO: assert that value is byte-array
+  (when-not (data/bytes? value)
+    (throw (ex-info (str "Bignums must be represented as a tagged byte array, got: "
+                         (class value))
+                    {:tag tag, :value value})))
   (let [big-integer (BigInteger. value)]
     (case tag
       2 (bigint big-integer)
@@ -33,15 +36,43 @@
 
 
 
+;; ## Decimal Fractions
+
+(defn format-big-decimal
+  [^BigDecimal value]
+  (let [exponent (.scale value)
+        mantissa (.unscaledValue value)]
+    (data/tagged-value 4 [(- exponent) mantissa])))
+
+
+(defn parse-big-decimal
+  [^long tag value]
+  (when (not= tag 4)
+    (throw (ex-info (str "Decimal fractions must be represented with tag 4, got: "
+                         tag)
+                    {:tag tag, :value value})))
+  (let [[exponent mantissa] value]
+    (BigDecimal. (biginteger mantissa) (- exponent))))
+
+
+
+;; ## Bigfloats
+
+;; Not Supported
+
+
+
 ;; ## Codec Formatter/Handler Maps
 
 (def number-formatters
   "Map of bignum types to render as tag 2/3 values."
-  {BigInteger format-big-int
-   BigInt format-big-int})
+  {BigInt     format-big-int
+   BigInteger format-big-int
+   BigDecimal format-big-decimal})
 
 
 (def number-handlers
   "Map of tag handlers to parse bignum values."
   {2 parse-big-int
-   3 parse-big-int})
+   3 parse-big-int
+   4 parse-big-decimal})
--- test/clj_cbor/tags/numbers_test.clj
+++ test/clj_cbor/tags/numbers_test.clj
@@ -5,15 +5,11 @@
     [clj-cbor.test-utils :refer :all]))
 
 
-;  | 23(h'01020304')              | 0xd74401020304                     |
-;  |                              |                                    |
-;  | 24(h'6449455446')            | 0xd818456449455446                 |
-;  |                              |                                    |
-;  | 32("http://www.example.com") | 0xd82076687474703a2f2f7777772e6578 |
-;  |                              | 616d706c652e636f6d                 |
-
-
 (deftest bignums
-  (testing "bignum"
-    (check-roundtrip  18446744073709551616N "C249010000000000000000")
-    (check-roundtrip -18446744073709551617N "C349010000000000000000")))
+  (check-roundtrip  18446744073709551616N "C249010000000000000000")
+  (check-roundtrip -18446744073709551617N "C349010000000000000000"))
+
+
+(deftest decimal-fractions
+  (check-roundtrip  273.15M "C48221196AB3")
+  (check-roundtrip 3.14159M "C482241A0004CB2F"))
