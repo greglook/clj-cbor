a2345566aa49ca1141621e7e5551fcd6ceff872f
Greg Look <greg@greg-look.net>
2016-12-20 23:16:31 -0800

    Update number tag tests.

--- test/clj_cbor/tags/numbers_test.clj
+++ test/clj_cbor/tags/numbers_test.clj
@@ -2,14 +2,21 @@
   (:require
     [clojure.test :refer :all]
     [clj-cbor.core :as cbor]
+    [clj-cbor.tags.numbers :refer :all]
     [clj-cbor.test-utils :refer :all]))
 
 
 (deftest bignums
-  (check-roundtrip  18446744073709551616N "C249010000000000000000")
-  (check-roundtrip -18446744073709551617N "C349010000000000000000"))
+  (let [codec (cbor/cbor-codec
+                :formatters number-formatters
+                :tag-handlers number-handlers)]
+    (check-roundtrip codec =  18446744073709551616N "C249010000000000000000")
+    (check-roundtrip codec = -18446744073709551617N "C349010000000000000000")))
 
 
 (deftest decimal-fractions
-  (check-roundtrip  273.15M "C48221196AB3")
-  (check-roundtrip 3.14159M "C482241A0004CB2F"))
+  (let [codec (cbor/cbor-codec
+                :formatters number-formatters
+                :tag-handlers number-handlers)]
+    (check-roundtrip codec =  273.15M "C48221196AB3")
+    (check-roundtrip codec = 3.14159M "C482241A0004CB2F")))
