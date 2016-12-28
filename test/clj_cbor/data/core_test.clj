78da4120348c44844352df1edb03769410186fc4
Greg Look <greg@greg-look.net>
2016-12-27 22:20:57 -0800

    Add basic data core test.

---
+++ test/clj_cbor/data/core_test.clj
@@ -0,0 +1,13 @@
+(ns clj-cbor.data.core-test
+  (:require
+    [clojure.test :refer :all]
+    [clj-cbor.data.core :as data]
+    [clj-cbor.test-utils :refer :all]))
+
+
+(deftest simple-value-construction
+  (is (data/simple-value? (data/simple-value 21)))
+  (is (thrown? IllegalArgumentException
+        (data/simple-value -1)))
+  (is (thrown? IllegalArgumentException
+        (data/simple-value 256))))
