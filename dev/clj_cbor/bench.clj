4e7631e69af0a1e5f65515d668f62f3d24c1b2be
Greg Look <greg@greg-look.net>
2017-01-07 09:20:45 -0800

    Add initial benchmarking ns.

---
+++ dev/clj_cbor/bench.clj
@@ -0,0 +1,13 @@
+(ns clj-cbor.bench
+  "Benchmarking utilities for format comparisons."
+  (:require
+    [cognitecht.transit :as transit]
+    [clojure.data.fressian :as fress]
+    [clojure.test.check.generators :as gen]
+    [criterium.core :as crit]
+    [taoensso.nippy :as nippy]))
+
+
+(defn -main
+  [& args]
+  ,,,)
