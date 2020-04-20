e5d6826577e64de71a98dfe689c64d70e4557a97
Greg Look <greg@mvxcvi.com>
2020-04-20 12:06:19 -0700

    Move equivalence checking into test-utils ns.

--- dev/user.clj
+++ dev/user.clj
@@ -9,3 +9,22 @@
     [clojure.stacktrace :refer [print-cause-trace]]
     [clojure.string :as str]
     [clojure.tools.namespace.repl :refer [refresh]]))
+
+
+;; FIXME: failing test cases:
+;; [#{##NaN}]
+;; [#{\space}]
+(defn round-trip-equivalent?
+  "True if the given value round-trips through CBOR to an 'equivalent' value.
+  Returns the decoded value on success, or throws an exception on failure."
+  ([value]
+   (round-trip-equivalent? cbor/default-codec value))
+  ([codec value]
+   (let [encoded (cbor/encode codec value)
+         decoded (cbor/decode encoded)]
+     (if (equivalent? value decoded)
+       decoded
+       (throw (ex-info "Data did not round-trip to an equivalent value"
+                       {:value value
+                        :encoded (bin->hex encoded)
+                        :decoded decoded}))))))
--- test/clj_cbor/generative_test.clj
+++ test/clj_cbor/generative_test.clj
@@ -1,79 +1,12 @@
 (ns clj-cbor.generative-test
-  "Decoding tests. Test examples are from RFC 7049 Appendix A."
+  "Generative tests for finding complex data values which may not round-trip
+  correctly."
   (:require
     [clj-cbor.core :as cbor]
-    [clj-cbor.test-utils :refer [bytes=]]
+    [clj-cbor.test-utils :refer [equivalent?]]
     [clojure.test.check.clojure-test :refer [defspec]]
     [clojure.test.check.generators :as gen]
-    [clojure.test.check.properties :as prop])
-  (:import
-    (java.util
-      List
-      Map
-      Set)
-    java.util.regex.Pattern))
-
-
-;; TODO: class-only multimethods should be protocols
-(defmulti equivalent?
-  (fn [a _] (class a)))
-
-
-(defmethod equivalent? :default
-  [a b]
-  (= a b))
-
-
-(defmethod equivalent? (class (byte-array 0))
-  [a b]
-  (bytes= a b))
-
-
-(defmethod equivalent? Character
-  [a b]
-  (= (str a) (str b)))
-
-
-(defmethod equivalent? Pattern
-  [a b]
-  (= (str a) (str b)))
-
-
-(defmethod equivalent? Number
-  [a b]
-  (if (Double/isNaN a)
-    (Double/isNaN b)
-    (= a b)))
-
-
-(defmethod equivalent? List
-  [a b]
-  (and (instance? List b)
-       (= (count a) (count b))
-       (every? true? (map equivalent? a b))))
-
-
-(defmethod equivalent? Set
-  [a b]
-  (and (instance? Set b)
-       (= (count a) (count b))
-       (every? #(equivalent? % (get b %)) a)))
-
-
-(defmethod equivalent? Map
-  [a b]
-  (and (instance? Map b)
-       (= (count a) (count b))
-       (loop [a a
-              b b]
-         (if-let [[k v] (first a)]
-           (if-let [[match-k match-v]
-                    (first (filter (comp (partial equivalent? k) key) b))]
-             (if (equivalent? v match-v)
-               (recur (dissoc a k) (dissoc b match-k))
-               false)
-             false)
-           (empty? b)))))
+    [clojure.test.check.properties :as prop]))
 
 
 (defspec ^:generative round-trip-equivalence 100
